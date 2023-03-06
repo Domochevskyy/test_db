31a7cc07d86e6c33da1e0bf64c545c2c7c507311
Nikolay Mednyi <n.mednyi@yadro.com>
2023-03-06 18:55:30 +0300

    Add simple db client

---
+++ common/db_client.py
@@ -0,0 +1,30 @@
+import psycopg
+from psycopg import OperationalError, Connection
+
+from common.logger import get_logger
+
+logger = get_logger('db_client')
+
+
+class DataBaseClient:
+
+    def __init__(self, connection_info: str):
+        self.connection_info = connection_info
+        self.connection = self.connect(connection_info)
+
+    def connect(self, connection_info: str) -> Connection:
+        """Connect to a database server and return a new `Connection` instance."""
+        # TODO: add check for connection errors for outer operations from other classes
+        # TODO: add reconnection logic
+        try:
+            connect = psycopg.connect(conninfo=self.connection_info)
+            logger.info(f'Connect to {self.connection_info}.')
+            return connect
+        except OperationalError:
+            logger.error(f'Cannot connect to {connection_info}.')
+            raise
+
+    def close(self) -> None:
+        """Close database connection."""
+        self.connection.close()
+        logger.info(f'Close connection {self.connection_info}.')
